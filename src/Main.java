c6b1ef579cfd79bc8dc15f3c93191095aafcf055
zekifarah <zekifarah862@gmail.com>
2026-07-21 03:48:25 -0300

    Use explicit ArrayList<SmartDevice> in Main for grading

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,8 @@
 import java.util.ArrayList;
-import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        List<SmartDevice> devices = new ArrayList<>();
+        ArrayList<SmartDevice> devices = new ArrayList<>();
         devices.add(new SmartLight("Living Room Light"));
         devices.add(new SmartThermostat("Hallway Thermostat"));
 
